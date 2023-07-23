afef7eb3ea6dcdbca05ea3e1a51dab3da82fa328
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-07-23 16:24:02 +0530

    Add files via upload

---
+++ Find tripels with zero sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    #Function to find triplets with zero sum.    
+    def findTriplets(self, arr, n):
+        arr.sort()
+        
+        for i in range(n-2):
+            j=i+1
+            k=n-1
+            
+            target=-arr[i]
+            while(j<k):
+                sum=arr[j]+arr[k]
+                if(sum==target):
+                    return 1
+                elif sum>target:
+                    k-=1
+                else:
+                    j+=1
+                    
+        return 0
