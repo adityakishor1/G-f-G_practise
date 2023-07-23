ac2dc54da9147bed9042a3079743a6d456471886
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-07-23 16:24:42 +0530

    Add files via upload

---
+++ Transpose of matrix.py
@@ -0,0 +1,6 @@
+class Solution:
+    def transpose(self, matrix, n):
+        for i in range(n):
+            for j in range(i+1,n):
+                #swap matrix[i][j] and matrix[j][i]
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
