d94ad3b6b1ecfbbae0d830feb0feb8371cb22397
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-09-17 18:53:29 +0530

    Add files via upload

---
+++ lucky number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isLucky(self, n):
+        def check(n,i):
+            if(n<i): return True
+            if(n%i==0): return False
+            
+            return check(n-n//i,i+1)
+        
+        return check(n,2)
