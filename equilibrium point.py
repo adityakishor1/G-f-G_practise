bed0cc3432b23ae62ddc0a37a65acda410aad935
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-09-25 16:44:45 +0530

    Add files via upload

---
+++ equilibrium point.py
@@ -0,0 +1,12 @@
+class Solution:
+    def equilibriumPoint(self,A, N):
+        sumArray=sum(A);
+        
+        ss=0
+        
+        for i in range(N):
+            if(ss==sumArray-ss-A[i]): return i+1;
+            
+            ss+=A[i]
+        
+        return -1
