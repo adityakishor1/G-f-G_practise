a973871e834a74313a4faa2114e3c6b0bf7fe1af
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-10-02 01:14:34 +0530

    Add files via upload

---
+++ BoundaryTraversal.py
@@ -0,0 +1,16 @@
+class Solution:
+    def BoundaryTraversal(self,matrix, n, m):
+       
+        ans=[]
+       
+        if(n==1):
+            for i in range(m): ans.append(matrix[0][i])
+        elif(m==1):
+            for i in range(n): ans.append(matrix[i][0])
+        else:
+            for i in range(m-1): ans.append(matrix[0][i])
+            for i in range(n-1): ans.append(matrix[i][m-1])
+            for i in range(m-1,0,-1): ans.append(matrix[n-1][i])
+            for i in range(n-1,0,-1): ans.append(matrix[i][0])
+        
+        return ans
