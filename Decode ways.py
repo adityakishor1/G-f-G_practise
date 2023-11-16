cababf9a3495bbd29557544ab80f3f7275e8cf36
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-11-16 23:34:59 +0530

    Add files via upload

---
+++ Decode ways.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numDecodings(self, s):
+        def helper(s, i):
+            if (s, i) in memo: return memo[(s, i)]
+            if i>=len(s): return 1
+            if s[i]=='0': return 0
+            
+            count = 0
+            count += helper(s, i+1)
+            if len(s)-i>=2 and int(s[i:i+2])<=26: count += helper(s, i+2)
+            memo[(s, i)] = count
+            return count
+                
+        memo = {}
+        return helper(s, 0)
