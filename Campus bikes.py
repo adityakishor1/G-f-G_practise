365eb0775d7684fb168b7ba71564a807727e650d
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-11-27 17:34:09 +0530

    Add files via upload

---
+++ Campus bikes.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def assignBikes(self, workers, bikes):
+        M = len(bikes)
+        N = len(workers)
+        costs = [[0]*N for _ in xrange(M)]
+        
+        for i in xrange(M):
+            for j in xrange(N):
+                costs[i][j] = abs(workers[j][0]-bikes[i][0])+abs(workers[j][1]-bikes[i][1])
+        
+        pq = [(0, '0'*M)]
+        visited = set()
+        while pq:
+            cost, state = heapq.heappop(pq)
+            if state in visited: continue
+            visited.add(state)
+            
+            j = state.count('1') #j users have selected bikes
+            if j>=N: return cost
+            
+            for i in xrange(M):
+                if state[i]=='1': continue
+                
+                nextState = state[:i] + '1' + state[i+1:]
+                if nextState in visited: continue
+                heapq.heappush(pq, (cost+costs[i][j], nextState))
+        
+        return float('inf')
