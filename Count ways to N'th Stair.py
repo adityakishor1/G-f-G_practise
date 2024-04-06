5ff8e1b4412e60142579a677d1bc942480423527
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2024-04-06 22:53:36 +0530

    Add files via upload

---
+++ Count ways to N'th Stair.py
@@ -0,0 +1,10 @@
+
+class Solution:
+
+    #Function to count number of ways to reach the nth stair
+    #when order does not matter.
+    def countWays(self, n):
+
+        mod = 1000000007
+        return 1+n//2
+        # code here
