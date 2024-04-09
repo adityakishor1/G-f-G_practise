d5389509a1d83398e750fc80b153786f7511f073
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2024-04-09 22:10:20 +0530

    Add files via upload

---
+++ Minimum Points To Reach Destination.py
@@ -0,0 +1,9 @@
+class Solution:
+	def minPoints(self, m, n, points):
+	    dp=[[float("inf") for _ in range(n+1)] for _ in range(m+1)]
+	    dp[m-1][n]=1
+	    dp[m][n-1]=1
+	    for i in range(m-1,-1,-1):
+	        for j in range(n-1,-1,-1):
+	            dp[i][j]=max(1,(min(dp[i+1][j],dp[i][j+1])-points[i][j]))
+	    return dp[0][0]
