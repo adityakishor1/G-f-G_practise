e70b08bc23abd442997c9fdf1b7dc678fa1cc9a6
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2024-05-30 21:44:49 +0530

    Add files via upload

---
+++ String Subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countWays(self, s1: str, s2: str) -> int:
+        MOD = int(1e9 + 7)
+        m, n = len(s1), len(s2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1):
+            dp[i][0] = 1
+        
+        # Count the subsequences
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % MOD
+                else:
+                    dp[i][j] = dp[i - 1][j]
+        
+        return dp[m][n]
