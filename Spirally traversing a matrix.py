2acf28abf13b948624cafcb96c015627939425a4
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2024-08-01 12:49:03 +0530

    Add files via upload

---
+++ Spirally traversing a matrix.py
@@ -0,0 +1,28 @@
+class Solution:
+    def spirallyTraverse(self, matrix):
+        result = []
+        if not matrix:
+            return result
+        
+        top, bottom = 0, len(matrix) - 1
+        left, right = 0, len(matrix[0]) - 1
+        
+        while top <= bottom and left <= right:
+            for i in range(left, right + 1):
+                result.append(matrix[top][i])
+            top += 1
+            for i in range(top, bottom + 1):
+                result.append(matrix[i][right])
+            right -= 1
+            
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    result.append(matrix[bottom][i])
+                bottom -= 1
+            
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    result.append(matrix[i][left])
+                left += 1
+        
+        return result
