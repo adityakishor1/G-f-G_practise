96a61b281b3497bd9c0c20123466215f093840c0
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2024-08-02 15:02:54 +0530

    Add files via upload

---
+++ editDistance.py
@@ -0,0 +1,18 @@
+class Solution:
+    def editDistance(self, str1, str2):
+        m, n = len(str1), len(str2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1):
+            dp[i][0] = i
+        for j in range(n + 1):
+            dp[0][j] = j
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if str1[i - 1] == str2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(dp[i - 1][j - 1],     
+                                   dp[i - 1][j],         
+                                   dp[i][j - 1]) + 1    
+
+        return dp[m][n]
