49691ae602a7122c87dad5ccbc66f9054cb41ea8
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2024-09-16 10:00:54 +0530

    Add files via upload

---
+++ Longest valid Parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxLength(self, s: str) -> int:
+        stack = [-1]
+        max_len = 0
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    max_len = max(max_len, i - stack[-1])
+        
+        return max_len
