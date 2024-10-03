e14c73a7569d0a9c88110cad2ca96c4770e9fe46
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2024-10-03 16:04:01 +0530

    Add files via upload

---
+++ Majority Vote.py
@@ -0,0 +1,34 @@
+
+class Solution:
+    def findMajority(self, nums):
+        if not nums:
+            return [-1]
+        candidate1, candidate2 = None, None
+        count1, count2 = 0, 0
+        
+        for num in nums:
+            if candidate1 == num:
+                count1 += 1
+            elif candidate2 == num:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = num, 1
+            elif count2 == 0:
+                candidate2, count2 = num, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        count1, count2 = 0, 0
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+        
+        result = []
+        n = len(nums)
+        if count1 > n // 3:
+            result.append(candidate1)
+        if count2 > n // 3:
+            result.append(candidate2)
+        return sorted(result) if result else [-1]
