3756d4893adf22d55348501fed8d9b6ed3b61680
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2024-10-14 18:15:29 +0530

    Add files via upload

---
+++ Count Linked List Nodes.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            last = self.head
+            while last.next:
+                last = last.next
+            last.next = new_node
+
+class Solution:
+    def getCount(self, head):
+        count = 0
+        current = head
+        while current:
+            count += 1
+            current = current.next
+        return count
