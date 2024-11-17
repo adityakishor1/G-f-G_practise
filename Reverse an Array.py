c7401bde58d4409a0df401c3493117b4f1e9ee6b
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2024-11-17 10:54:48 +0530

    Add files via upload

---
+++ Reverse an Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseArray(self, arr):
+        left, right = 0, len(arr) - 1
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+        return arr
